85e299334cf78c62a155b9cf9820286e4a57de5b
CherryB331 <brianna.cherry@salem.edu>
2026-04-10 16:53:41 -0400

    15-starting on java

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+document.getElementById("home-btn").addEventListener("click", function() {
+    window.location.href = "/Users/briannacherry/FinalProject/index.html";
+})
+document.getElementById("resources-btn").addEventListener("click", function() {
+    window.location.href = "/Users/briannacherry/FinalProject/Resources.html";
+})
+document.getElementById("thingstodo-btn").addEventListener("click", function() {
+    window.location.href = "/Users/briannacherry/FinalProject/Thingstodo.html";
+})
+document.getElementById("possibilities-btn").addEventListener("click", function() {
+    window.location.href = "/Users/briannacherry/FinalProject/Possibilities.html";
+})
 
