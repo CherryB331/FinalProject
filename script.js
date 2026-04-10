3b1c79215a8eb33022f6dc26f8b3e4bd7db59d31
CherryB331 <brianna.cherry@salem.edu>
2026-04-10 17:30:46 -0400

    15e

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-console.log(document.getElementById("homeBtn"));
-
-
-
-document.getElementById("homeBtn").addEventListener("click", function() {
-    window.location.href = "index.html";
-})
-document.getElementById("resourcesBtn").addEventListener("click", function() {
-    window.location.href = "Resources.html";
-})
-document.getElementById("thingstodoBtn").addEventListener("click", function() {
-    window.location.href = "Thingstodo.html";
-})
-
-
+document.querySelectorAll('.navBtn').forEach(button => {
+    button.addEventListener('click', () => { window.location.href = button.dataset.link });
+});
+        
 
