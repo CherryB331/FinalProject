f30979035a1770e9058c902d5f36ac0321ec3f3c
CherryB331 <brianna.cherry@salem.edu>
2026-04-10 17:44:51 -0400

    15f

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 document.querySelectorAll('.navBtn').forEach(button => {
     button.addEventListener('click', () => { window.location.href = button.dataset.link });
 });
-        
 
+
+fetch("https://worldtimeapi.org/api/ip")
+  .then(res => res.json())
+  .then(data => {
+    const apiTime = new Date(data.datetime);
+    document.getElementById('datetime').textContent = apiTime.toLocaleString();
+  });
